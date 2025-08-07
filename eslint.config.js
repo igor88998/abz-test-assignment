a8efc8134e9b225ee51eb081287e4ee4fd1130d8
Ihor Makoviichuk <makoviichuk@involve.software>
2025-08-08 02:59:57 +0300

    Update ESLint configuration to include feature-sliced plugin and adjust rules; add new dependencies in package.json and package-lock.json

--- eslint.config.js
+++ eslint.config.js
@@ -3,24 +3,49 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import featureSliced from '@conarti/eslint-plugin-feature-sliced'
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+export default tseslint.config(
+  {
+    ignores: ['dist'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'feature-sliced': featureSliced,
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     rules: {
+      ...reactHooks.configs.recommended.rules,
+      ...reactRefresh.configs.vite.rules,
       '@typescript-eslint/no-explicit-any': 'off',
+      'feature-sliced/layers-slices': 'error',
+      'feature-sliced/absolute-relative': 'error',
+      'feature-sliced/public-api': 'error',
+    },
+    settings: {
+      'feature-sliced': {
+        layers: ['app', 'pages', 'widgets', 'features', 'entities', 'shared'],
+      },
     },
   },
-])
+  {
+    files: ['src/shared/**/*.{ts,tsx}'],
+    rules: {
+      'feature-sliced/absolute-relative': 'off',
+    },
+  },
+  {
+    files: ['src/app/**/*.{ts,tsx}'],
+    rules: {
+      'feature-sliced/absolute-relative': 'off',
+    },
+  }
+)
